b5de4b979fdc327042aa969ce960856589822b33
am2011 <adel.meme.philo@gmail.com>
2022-07-01 17:33:59 -0700

    6-print_numberz.c adde

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry Point
+ *
+ * Return:Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	int n = 48;
+
+	while (n <= 57)
+
+	{
+
+		putchar(n);
+
+		n += 1;
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
