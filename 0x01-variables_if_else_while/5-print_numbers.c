d43616d9b55d9c07d324154db1a755eb622a619c
am2011 <adel.meme.philo@gmail.com>
2022-07-01 17:28:54 -0700

    5-print_numbers.c adde

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	int n;
+
+	while (n < 10)
+
+	{
+
+		printf("%d", n);
+
+		n += 1;
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
