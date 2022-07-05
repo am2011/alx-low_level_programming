63b3dbf1e74faebdc2dbe0c625827d2ee06f702b
am2011 <adel.meme.philo@gmail.com>
2022-07-05 14:07:59 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,5 @@
 #include "main.h"
-#include <string.h>
-#ifndef MAIN_H
-#define MAIN_H
 
-void _putchar(char str);
-#endif
 
 
 /**
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,3 @@
-#include <stdio.h>
 
-
-
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int nld);
-void jack_bauer(void);
-void times_table(void);
-int add(int one, int two);
-void print_to_98(int n);
-void print_times_table(int n);
-void _putchar(char str)
-{
-
-printf("%c",str);
-
-}
+int _putchar(char str);
 
