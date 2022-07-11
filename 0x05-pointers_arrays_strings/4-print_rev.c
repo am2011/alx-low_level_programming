8cf636ef644d1cb6200e69e57ddd834f55c51534
am2011 <adel.meme.philo@gmail.com>
2022-07-11 16:25:25 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,18 +5,18 @@
  */
 void print_rev(char *s)
 {
-	int i,n;
+	int i, n;
 	int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		count++;
 	}
-	
+
 	for (n = (count - 1); n >= 0; n--)
 	{
 		_putchar(s[n]);
-		
+
 	}
 	_putchar('\n');
 }
