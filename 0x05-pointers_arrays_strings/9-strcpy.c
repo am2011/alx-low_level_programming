c2f18fccb4f2b0c5127ceb09cb9b1622f348b7f8
am2011 <adel.meme.philo@gmail.com>
2022-07-11 17:50:45 -0700

    9-strcpy.c added

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 /**
- * strcpy - copies string pointed to by src to buffer pointed to by dest
+ * *_strcpy - copies string pointed to by src to buffer pointed to by dest
  * @dest: the pointer to a string
  * @src: the pointer to a buffer
  * Return: the value of the pointer to dest.
