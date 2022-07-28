bc6638de03c33e6add33d1191bff8dff0148e551
am2011 <adel.meme.philo@gmail.com>
2022-07-28 16:37:45 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,47 @@
-#include <stdlib.h>
-#include <stdio.h>
-/**
- * string_nconcat - concatenate two strings up to n bytes.
- * @s1: source string
- * @s2: string to truncate up to n bytes
- * @n: number of bytes to truncate by
- * Return: pointer to new buffer
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *p;
-	unsigned int s1count, s2count, sizeBuffer, i;
+#include <stdlib.h> 
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (s1count = 0; s1[s1count]; s1count++)
-		;
-	for (s2count = 0; s2[s2count]; s2count++)
-		;
-	s2count > n ? (s2count = n) : (n = s2count);
-	sizeBuffer = s1count + s2count + 1;
-	p = malloc(sizeBuffer * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < sizeBuffer - 1; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	p[sizeBuffer] = '\0';
-	return (p);
+/** 
+  * string_nconcat - function that concatenates two strings 
+  * @s1: input string 1 
+  * @s2: input string 2 
+  * @n: first n bytes 
+  * Return: char 
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n) 
+{ 
+	char *str; 
+	char *nul = ""; 
+	unsigned int x, y; 
+  
+	x = y = 0; 
+	if (s1 == NULL) 
+		s1 = nul; 
+	if (s2 == NULL) 
+		s2 = nul; 
+	while (s1[x] != '\0') 
+		x += 1; 
+	while (s2[y] != '\0') 
+		y += 1; 
+	y++; 
+	if (n >= y) 
+		n = y; 
+	str = malloc(sizeof(*str) * n + (x + 1)); 
+	if (str == NULL) 
+		return (NULL); 
+	x = 0; 
+	while (s1[x] != '\0') 
+	{ 
+		str[x] = s1[x]; 
+		x += 1; 
+	} 
+	y = 0; 
+	while (s2[y] != '\0' && y < n) 
+	{ 
+		str[x] = s2[y]; 
+		x++; 
+		y++; 
+	} 
+	str[x] = '\0'; 
+	return (str); 
 }
