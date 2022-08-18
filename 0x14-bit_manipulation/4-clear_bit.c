867777b10dfa2291737fd505acf4d7c289d6875a
am2011 <adel.meme.philo@gmail.com>
2022-08-18 16:40:28 -0700

    4-clear_bit.c update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *  Clear_bit - sets a bit at index to zero
+ *  clear_bit - sets a bit at index to zero
  *  @n: pointer to number
  *  @index: index to change
  *  Return: 1 on success, -1 on failure
