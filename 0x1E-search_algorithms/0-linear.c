35606379fb0d1b3702418c1d58bfb6cd57c66583
am2011 <adel.meme.philo@gmail.com>
2023-01-27 17:13:21 -0800

    fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,17 +8,17 @@
  *  @value: value to search in
  *  Return: Always EXIT_SUCCESS
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL)
 		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
